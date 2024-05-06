53307b50c298aab98154f6bd04415ec9787b52b7
Yuki Ito <taitanpoton@gmail.com>
2024-05-06 18:28:52 +0900

    mainのプログラムに説明を追加

--- Classification/main_cls_cv.py
+++ Classification/main_cls_cv.py
@@ -7,7 +7,7 @@ import time
 import torch
 import torch.nn as nn
 from data.sparseloader import DataLoader
-from data.data import LibSVMData, LibCSVData, CriteoCSVData
+from data.data import LibSVMData, LibCSVData, CriteoCSVData, MYCSVData
 from data.sparse_data import LibSVMDataSp
 from models.mlp import MLP_1HL, MLP_2HL, MLP_3HL
 from models.dynamic_net import DynamicNet, ForwardType
@@ -65,6 +65,10 @@ def get_data():
     elif opt.data == 'yahoo.pair':
         train = LibCSVData(opt.tr, opt.feat_d)
         test = LibCSVData(opt.te, opt.feat_d)
+    # csvファイルを読み込み
+    elif opt.data == 'my_csv_data':
+        train = MYCSVData(opt.tr, opt.feat_d, label_col=1)
+        test = MYCSVData(opt.te, opt.feat_d, label_col=1)
     else:
         pass
 
@@ -107,7 +111,7 @@ def accuracy(net_ensemble, test_loader):
     loss = 0
     for x, y in test_loader:
         if opt.cuda:
-            x, y = x.cuda(), y.cuda()
+            x, y = x.float().cuda(), y.float().cuda()
         with torch.no_grad():
             middle_feat, out = net_ensemble.forward(x)
         correct += (torch.sum(y[out > 0.] > 0) + torch.sum(y[out < .0] < 0)).item()
@@ -120,7 +124,7 @@ def logloss(net_ensemble, test_loader):
     loss_f = nn.BCEWithLogitsLoss() # Binary cross entopy loss with logits, reduction=mean by default
     for x, y in test_loader:
         if opt.cuda:
-            x, y= x.cuda(), y.cuda().view(-1, 1)
+            x, y= x.float().cuda(), y.float().cuda().view(-1, 1)
         y = (y + 1) / 2
         with torch.no_grad():
             _, out = net_ensemble.forward(x)
@@ -135,7 +139,7 @@ def auc_score(net_ensemble, test_loader):
     posterior = []
     for x, y in test_loader:
         if opt.cuda:
-            x = x.cuda()
+            x = x.float().cuda()
         with torch.no_grad():
             _, out = net_ensemble.forward(x)
         prob = 1.0 - 1.0 / torch.exp(out)   # Why not using the scores themselve than converting to prob
@@ -159,8 +163,10 @@ def init_gbnn(train):
 
 if __name__ == "__main__":
 
+    #データ読み込み
     train, test, val = get_data()
     print(opt.data + ' training and test datasets are loaded!')
+    #学習データとテストデータをDataLoaderで分割
     train_loader = DataLoader(train, opt.batch_size, shuffle = True, drop_last=False, num_workers=2)
     test_loader = DataLoader(test, opt.batch_size, shuffle=False, drop_last=False, num_workers=2)
     if opt.cv:
@@ -170,8 +176,11 @@ if __name__ == "__main__":
     val_score = best_score
     best_stage = opt.num_nets-1
 
+    #データ数の多い方を予測した結果（データの偏り）
     c0 = init_gbnn(train)
+    #動的ニューラルネットワーク（Neural Boostingのこと）
     net_ensemble = DynamicNet(c0, opt.boost_rate)
+    
     loss_f1 = nn.MSELoss(reduction='none')
     loss_f2 = nn.BCEWithLogitsLoss(reduction='none')
     loss_models = torch.zeros((opt.num_nets, 3))
@@ -192,10 +201,13 @@ if __name__ == "__main__":
         train_loader = DataLoader(train, opt.batch_size, sampler = train_sampler, drop_last=True, num_workers=2)
         ################################################################################################
 
+        #MLP_2HL：2つの隠れ層を持つ．1つ目の隠れ層と2つ目の隠れ層の間に活性化関数が存在
         model = MLP_2HL.get_model(stage, opt)  # Initialize the model_k: f_k(x), multilayer perception v2
         if opt.cuda:
+            #gpuメモリに転送している
             model.cuda()
 
+        # 最適化アルゴリズム=Adam
         optimizer = get_optim(model.parameters(), opt.lr, opt.L2)
         net_ensemble.to_train() # Set the models in ensemble net to train mode
 
@@ -203,44 +215,55 @@ if __name__ == "__main__":
         for epoch in range(opt.epochs_per_stage):
             for i, (x, y) in enumerate(train_loader):
                 if opt.cuda:
-                    x, y= x.cuda(), y.cuda().view(-1, 1)
+                    print("x",x.shape)
+                    print("y",y.shape)
+                    #x, yをそれぞれgpuメモリ上に転送
+                    x, y= x.float().cuda(), y.float().cuda().view(-1, 1)
+                #順伝播計算をしている．middle_featが中間の特徴量，outが最終出力
                 middle_feat, out = net_ensemble.forward(x)
                 out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
+                #層の深さによって計算方法が異なる．重みの更新
                 if opt.model_order=='first':
                     grad_direction = y / (1.0 + torch.exp(y * out))
                 else:
-                    h = 1/((1+torch.exp(y*out))*(1+torch.exp(-y*out)))
+                    h = 1/((1+torch.exp(y*out))*(1+torch.exp(-y*out))).float()
                     grad_direction = y * (1.0 + torch.exp(-y * out))
                     out = torch.as_tensor(out)
                     nwtn_weights = (torch.exp(out) + torch.exp(-out)).abs()
-                _, out = model(x, middle_feat)
-                out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
-                loss = loss_f1(net_ensemble.boost_rate*out, grad_direction)  # T
+                _, out = model(x.float(), middle_feat)
+                out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1).float()
+                #lossを計算
+                loss = loss_f1(net_ensemble.boost_rate*out, grad_direction) # T
                 loss = loss*h
                 loss = loss.mean()
-                model.zero_grad()
-                loss.backward()
-                optimizer.step()
-                stage_mdlloss.append(loss.item()) 
+                model.zero_grad() #モデルの勾配をゼロにリセット
+                loss.backward() #逆伝播
+                optimizer.step() #パラメータの更新
+                stage_mdlloss.append(loss.item()) #訓練中に計算されたlossをリストへ追加
 
+        #モデルを追加している．
         net_ensemble.add(model)
         sml = np.mean(stage_mdlloss)
 
 
         stage_loss = []
         lr_scaler = 2
-        # fully-corrective step
+        # fully-corrective step 動的アンサンブル学習
+        # 過去のステージのモデル予測誤差を修正するために利用
         if stage != 0:
-            # Adjusting corrective step learning rate 
+            # Adjusting corrective step learning rate 15の倍数のときに学習率・L2正則化項が2倍になる．
             if stage % 15 == 0:
                 #lr_scaler *= 2
                 opt.lr /= 2
                 opt.L2 /= 2
             optimizer = get_optim(net_ensemble.parameters(), opt.lr / lr_scaler, opt.L2)
+            
             for _ in range(opt.correct_epoch):
                 for i, (x, y) in enumerate(train_loader):
                     if opt.cuda:
-                        x, y = x.cuda(), y.cuda().view(-1, 1)
+                        x, y = x.float().cuda(), y.float().cuda().view(-1, 1)
+                    
+                    #モデルにxを入力
                     _, out = net_ensemble.forward_grad(x)
                     out = torch.as_tensor(out, dtype=torch.float32).cuda().view(-1, 1)
                     y = (y + 1.0) / 2.0
@@ -250,14 +273,16 @@ if __name__ == "__main__":
                     optimizer.step()
                     stage_loss.append(loss.item())
 
-        
+        #テストデータセットを用いてアンサンブルモデルのロスを計算
         sl_te = logloss(net_ensemble, test_loader)
         # Store dynamic boost rate
         dynamic_br.append(net_ensemble.boost_rate.item())
         # store model
         net_ensemble.to_file(opt.out_f)
+        # データを読み出して，動的アンサンブルモデルを維持
         net_ensemble = DynamicNet.from_file(opt.out_f, lambda stage: MLP_2HL.get_model(stage, opt))
 
+        #1ステージに要した時間を計算
         elapsed_tr = time.time()-t0
         sl = 0
         if stage_loss != []:
